288aa1c5c6105c9633fe499c685d2dde6cb4b445
田中涼誠 <iesoyr907@icloud.com>
2018-12-09 14:11:55 +0900

    Hi! I made a simple stopwatch application! Please review!

---
+++ STOPWATCH/StartViewController.swift
@@ -0,0 +1,30 @@
+//
+//  StartViewController.swift
+//  STOPWATCH
+//
+//  Created by Ryosei Tanaka on 2018/12/09.
+//  Copyright © 2018 Ryosei Tanaka. All rights reserved.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- STOPWATCH/ViewController.swift
+++ STOPWATCH/ViewController.swift
@@ -9,12 +9,51 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet var label: UILabel!
+    
+    var count: Float = 0.00
+    
+    var timer: Timer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @objc func up(){
+        count = count + 0.01
+        label.text = String(format:"%.2f",count)
+    }
+    
+    @IBAction func reset(){
+        if timer.isValid{
+            timer.invalidate()
+        }
+        count = 0.00
+        
+        label.text = String(format: "%.2f",count)
+    }
+    @IBAction func start(){
+        if !timer.isValid {
+            timer = Timer.scheduledTimer(timeInterval: 0.01,
+                                         target:self,
+                                         selector: #selector(self.up),
+                                         userInfo: nil,
+                                         repeats:true)
+        }
+    }
+    
+    @IBAction func stop(){
+        if timer.isValid {
+            timer.invalidate()
+            
+        }
+        
+    }
 }
-
